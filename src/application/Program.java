c6665a924ac1df8eb1845c6f5695e2df7a9590c8
Allan Tiotonho Pereira <allanpereira773@gmail.com>
2023-02-21 17:51:38 -0300

    Seller entity class

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,12 +1,18 @@
 package application;
+import java.util.Date;
+
 import model.entities.Department;
+import model.entities.Seller;
 
 public class Program {
 
 	public static void main(String[] args) {
 		
 		Department dp = new Department(1001, "Books");
-		System.out.println(dp);
+		
+		Seller seller = new Seller(1, "Antonio", "antonio@gmail.com", new Date(), 5000.0, dp);
+		
+		System.out.println(seller);
 	}
 
 }
