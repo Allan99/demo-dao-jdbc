1c9d6f7988c4161118d657063b6471c9ff0d8f01
Allan Tiotonho Pereira <allanpereira773@gmail.com>
2023-02-23 18:43:19 -0300

    insert implementation

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,4 +1,5 @@
 package application;
+import java.util.Date;
 import java.util.List;
 
 import model.dao.DaoFactory;
@@ -28,6 +29,11 @@ public class Program {
 		for(Seller s: Allsellers) {
 			System.out.println(s);
 		}
+		
+		System.out.println("\n=== Teste 4: Seller insert ====");
+		Seller newSeller = new Seller(null, "Greg", "greg@gmail.com", new Date(), 4000.0, dep);
+		sellerDao.insert(newSeller);
+		System.out.println("inserted! new id = " + newSeller.getId());
 	}
 
 }
